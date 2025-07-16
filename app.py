d958ca6dfa9f03ea3fb2336d2a6f09aa5b77e7f5
criiz-prjcts <criiz.random@gmail.com>
2025-07-16 08:42:00 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -14,14 +14,49 @@ LANG_EMOJIS = {
 
 # Diccionarios de codificación (abreviado por brevedad, reemplazar por los completos)
 CODES = {
+    "Parsel": {
+        'A': 'Esh', 'B': 'Ch', 'C': 'Eish', 'D': 'Shi', 'E': 'Ash', 'F': 'Asha', 'G': 'Ei',
+        'H': 'Shis', 'I': 'Osh', 'J': 'Xim', 'K': 'Ss', 'L': 'Suh', 'M': 'Xan', 'N': 'Sh',
+        'O': 'Ush', 'P': 'Cah', 'Q': 'Xii', 'R': 'In', 'S': 'Shs', 'T': 'Cass', 'U': 'Ish',
+        'V': 'Aus', 'W': 'Xi', 'X': 'Shh', 'Y': 'Sss', 'Z': 'Xiy'
+    },
+    "Troll": {
+        'A': 'Oet', 'B': 'Lool', 'C': 'Cad', 'D': 'Tuu', 'E': 'Fno', 'F': 'Oll', 'G': 'Cawl',
+        'H': 'Tel', 'I': 'Lot', 'J': 'Tro', 'K': 'Ojt', 'L': 'Rew', 'M': 'Pit', 'N': 'Graw',
+        'O': 'Ietg', 'P': 'Zit', 'Q': 'Vlq', 'R': 'Pos', 'S': 'Velf', 'T': 'Insl', 'U': 'Dil',
+        'V': 'Grig', 'W': 'Bet', 'X': 'Rot', 'Y': 'Etss', 'Z': 'Sor'
+    },
+    "Sirenio": {
+        'A': 'Ogl', 'B': 'Jig', 'C': 'Sul', 'D': 'Fyg', 'E': 'Igl', 'F': 'Tigl', 'G': 'Fegl',
+        'H': 'Sug', 'I': 'Ugl', 'J': 'Leg', 'K': 'Jhul', 'L': 'Wag', 'M': 'Poh', 'N': 'Pluh',
+        'Ñ': 'Peeg', 'O': 'Agl', 'P': 'Degl', 'Q': 'Zel', 'R': 'Tig', 'S': 'Xuu', 'T': 'Geg',
+        'U': 'Egl', 'V': 'Dafh', 'W': 'Dufh', 'X': 'Lhh', 'Y': 'Xieh', 'Z': 'Welg'
+    },
+    "Duendigonza": {
+        'A': 'Pot', 'B': 'Uvo', 'C': 'Dv', 'D': 'Kok', 'E': 'Gvc', 'F': 'Bok', 'G': 'Tup',
+        'H': 'Qir', 'I': 'A', 'J': 'Sks', 'K': 'Vb', 'L': 'Bl', 'M': 'Num', 'N': 'Pon',
+        'O': 'Ak', 'P': 'Bl', 'Q': 'Nxz', 'R': 'Yuf', 'S': 'Crek', 'T': 'Ors', 'U': 'Ec',
+        'V': 'Osf', 'W': 'Zok', 'X': 'Voc', 'Y': 'Ilk', 'Z': 'Bgt'
+    },
+    "Draconis": {
+        'A': 'Erd', 'B': 'Rui', 'C': 'Dro', 'D': 'Edra', 'E': 'Ard', 'F': 'Ad', 'G': 'Dir',
+        'H': 'Udo', 'I': 'Ord', 'J': 'Dota', 'K': 'Kiu', 'L': 'Rad', 'M': 'Pro', 'N': 'Rer',
+        'Ñ': 'Ruy', 'O': 'Urd', 'P': 'Dri', 'Q': 'Uga', 'R': 'Rod', 'S': 'Pir', 'T': 'Yerd',
+        'U': 'Ird', 'V': 'Rao', 'W': 'Cra', 'X': 'Da', 'Y': 'Rri', 'Z': 'Zrag'
+    },
+    "Veela": {
+        'A': 'Lun', 'B': 'Zyr', 'C': 'Fae', 'D': 'Sil', 'E': 'Eth', 'F': 'Nym', 'G': 'Lor',
+        'H': 'Cel', 'I': 'Isl', 'J': 'Vyn', 'K': 'Syr', 'L': 'Ael', 'M': 'Rhe', 'N': 'Mor',
+        'O': 'Oph', 'P': 'Thal', 'Q': 'Qua', 'R': 'Sha', 'S': 'Nae', 'T': 'Del', 'U': 'Ume',
+        'V': 'Vel', 'W': 'Wyn', 'X': 'Xil', 'Y': 'Yse', 'Z': 'Zel'
+    },
     "Finofaudio": {
         'A': 'fi', 'B': 'fgh', 'C': 'fjd', 'D': 'fsa', 'E': 'fa', 'F': 'fre',
         'G': 'fes', 'H': 'fag', 'I': 'fu', 'J': 'feg', 'K': 'fo', 'L': 'fffg',
         'M': 'fur', 'N': 'fot', 'O': 'fe', 'P': 'for', 'Q': 'fafe', 'R': 'faer',
         'S': 'fiuf', 'T': 'ful', 'U': 'fo', 'V': 'feik', 'W': 'fyw', 'X': 'feg',
         'Y': 'feh', 'Z': 'fff'
-    },
-    # Agrega los demás idiomas aquí...
+    }
 }
 
 class TrieNode:
